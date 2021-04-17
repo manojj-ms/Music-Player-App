550b54f35310d074b07827436f0ef360874747dd
manoj <manojkumarmsd9204@gmail.com>
2021-04-17 10:50:01 +0530

    apk

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -62,11 +62,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Consumer<CatalogModel>(
         builder: (context, catalog, child) {
           if (catalog.items.isNotEmpty) {
